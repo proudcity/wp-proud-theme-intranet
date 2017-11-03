088f628f7d3e0979f75557bd646ba934f8d6b9a5
Jeff Lyon <jeff@albatrossdigital.com>
2017-11-02 21:45:39 -0700

    Theme updates

--- page.php
+++ page.php
@@ -1,8 +1,7 @@
 <?php while (have_posts()) : the_post(); ?>
     <?php get_template_part('templates/page', 'header'); ?>
     <div class="row">
-        <div class="col-sm-9"><?php get_template_part('templates/content', 'page'); ?></div>
-        <div class="col-sm-3"><?php dynamic_sidebar('sidebar-buddypress'); ?></div>
+        <div class="col-sm-12"><?php get_template_part('templates/content', 'page'); ?></div>
     </div>
 <?php endwhile; ?>
 <style>
